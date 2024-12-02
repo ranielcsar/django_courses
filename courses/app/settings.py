149c4fcbb5f1e39e378bce07d9b3c964c4a377a9
ranoob <ranielcsardev@gmail.com>
2024-12-02 15:33:10 -0300

    config: adicionado novo diretório de templates

--- courses/app/settings.py
+++ courses/app/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "app/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
