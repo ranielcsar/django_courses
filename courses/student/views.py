d08f5e7109287070aec419b54ae0dc9378f6b3e7
ranoob <ranielcsardev@gmail.com>
2024-12-02 15:34:39 -0300

    feat: view do aluno

--- courses/student/views.py
+++ courses/student/views.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 from django.shortcuts import render
+from teacher.models import Course, StatusEnum
 
 
-# Create your views here.
-def home(request):
-    return render(request, "index.html", {"student": request.user})
+@login_required
+def home(request: HttpRequest):
+    courses = Course.objects.filter(status=StatusEnum.ACTIVE.value)
+
+    return render(
+        request,
+        "index.html",
+        {
+            "student": request.user,
+            "courses": courses,
+        },
+    )
